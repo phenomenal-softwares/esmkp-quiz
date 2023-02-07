95ce7418f696f41fbf55d35b2c566563ab3382c6
phenomenal-softwares <115654413+phenomenal-softwares@users.noreply.github.com>
2023-02-07 22:19:35 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of the chapter president?",
+    answer: "Julius John Kehinde",
+    options: [
+      "Kehinde Julius John",
+      "John Julius Kehinde",
+      "Julius Kehinde John",
+      "Julius John Kehinde"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the title of the sermon preached by Bro Olarewaju Daniel on Feb 5, 2023?",
+    answer: "Comfort in the storm",
+    options: [
+      "Comforted in the storm",
+      "Comforted by the storm",
+      "Comfort in the storm",
+      "Comfort during the storm"
+    ]
+  },
+    {
+    numb: 3,
+    question: "'He considered the reproach of Christ greater wealth than the treasures of Egypt, for he was looking to the reward.'.. Who is the Bible talking about?",
+    answer: "Moses",
+    options: [
+      "Joseph",
+      "Jacob",
+      "Paul",
+      "Moses"
+    ]
+  },
+    {
+    numb: 4,
+    question: "For I know the thoughts that I think toward you, saith the Lord , thoughts of peace, and not of evil, to give you an expected end",
+    answer: "Jeremiah 29:11",
+    options: [
+      "Jeremiah 29:12",
+      "Jeremiah 29:19",
+      "Jeremiah 29:11",
+      "Jeremiah 29:13"
+    ]
+  },
+    {
+    numb: 5,
+    question: "And ye shall know the truth, and the truth shall ____ you free. John 8:32 KJV",
+    answer: "make",
+    options: [
+      "set",
+      "make",
+      "maketh",
+      "call"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How old was Manasseh when he began to reign?",
+    answer: "twelve",
+    options: [
+      "twelve",
+      "eight",
+      "twenty",
+      "twenty-one"
+    ]
+  },
+    {
+    numb: 7,
+    question: "The lines are fallen unto me in pleasant places ; yea, I have a goodly heritage.",
+    answer: "Psalm 16:6",
+    options: [
+      "Psalm 45:7",
+      "Psalm 16:6",
+      "Psalm 18:18",
+      "Psalm 6:16"
+    ]
+  },
+    {
+    numb: 8,
+    question: "For it is not possible that the blood of ___ and of ___ should take away sins. Hebrews 10:4 KJV",
+    answer: "bulls, goats",
+    options: [
+      "pigeons, goats",
+      "sheep, birds",
+      "fowls, rams",
+      "bulls, goats"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Thus saith the Lord; Cursed be the man that trusteth in man,...",
+    answer: "Jeremiah 17:5",
+    options: [
+      "Jeremiah 17:5",
+      "Jeremiah 5:2",
+      "Jeremiah 15:5",
+      "Jeremiah 1:7"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Yet ___ the archangel, when contending with the devil he disputed about the body of Moses,...Jude 1:9 KJV",
+    answer: "Michael",
+    options: [
+      "Michael",
+      "Gabriel",
+      "Emmanuel",
+      "Afoda Noah"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
