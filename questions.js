4c5cf3dee9f58734254a23ea43bb4d2137304632
phenomenal-softwares <115654413+phenomenal-softwares@users.noreply.github.com>
2023-02-18 19:36:26 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many are the musical alphabet?",
+    answer: "7",
+    options: [
+      "7",
+      "12",
+      "25",
+      "26"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the highest female singing voice?",
+    answer: "Soprano",
+    options: [
+      "Soprano",
+      "Mezzo-soprano",
+      "Alto",
+      "Tenor"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What vocal range is a male singer that can use head voice to sing as high as a female?",
+    answer: "Countertenor",
+    options: [
+      "Soprano",
+      "Tenor",
+      "Countertenor",
+      "Mezzo-soprano"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the male vocal range between Tenor and Bass?",
+    answer: "Baritone",
+    options: [
+      "Alto",
+      "Countertenor",
+      "Mezzo-soprano",
+      "Baritone"
+    ]
+  },
+    {
+    numb: 5,
+    question: "There are how many keys in music?",
+    answer: "12",
+    options: [
+      "7",
+      "8",
+      "10",
+      "12"
+    ]
+  },
+    {
+    numb: 6,
+    question: "___ is the arrangement of notes or voices one after another",
+    answer: "Melody",
+    options: [
+      "Melody",
+      "Harmony",
+      "Rhythm",
+      "Beat"
+    ]
+  },
+    {
+    numb: 7,
+    question: "A person that does not sing according to the rhythm of a song is said to sing ___?",
+    answer: "off-beat",
+    options: [
+      "off-beat",
+      "off-key",
+      "off-pitch",
+      "solemn"
+    ]
+  },
+    {
+    numb: 8,
+    question: "___ is created when different vocal parts of a choir are sung together",
+    answer: "Harmony",
+    options: [
+      "Melody",
+      "Rhythm",
+      "Key signature",
+      "Harmony"
+    ]
+  },
+    {
+    numb: 9,
+    question: "___ is how slow or fast a song is",
+    answer: "Tempo",
+    options: [
+      "Meter",
+      "Tempo",
+      "Beat",
+      "Speed"
+    ]
+  },
+    {
+    numb: 10,
+    question: "___ a high-range, very high-frequency tone typically used by boys",
+    answer: "Treble",
+    options: [
+      "Soprano",
+      "Tenor",
+      "Countertenor",
+      "Treble"
+    ]
+  },
+
+];
